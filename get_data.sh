996e44010ef9567ef8151a776201f1a573f3bb21
espes <espes@pequalsnp.com>
2016-08-03 11:38:46 -0700

    continue download

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p dataset/camera dataset/log
 
-wget -nc https://archive.org/download/comma-dataset/comma-dataset.zip
+wget --continue https://archive.org/download/comma-dataset/comma-dataset.zip
 mkdir -p dataset
 cd dataset
 unzip ../comma-dataset.zip
